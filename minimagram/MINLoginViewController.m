bb5ec37cfc223177e6d3a5b9dcad0a3aa772260e
Gianni Settino <g@miltonandparc.com>
2015-01-30 00:17:18 -0500

    Nail login workflow

--- minimagram/MINLoginViewController.m
+++ minimagram/MINLoginViewController.m
@@ -9,6 +9,7 @@
 #import "MINLoginViewController.h"
 #import "MINLoginView.h"
 #import "MINWebViewController.h"
+#import "MINTimelineTableViewController.h"
 
 @interface MINLoginViewController ()
 
@@ -26,11 +27,18 @@
     [super viewDidLoad];
     
     [self.view.loginButton addTarget:self action:@selector(loginButtonPressed) forControlEvents:UIControlEventTouchUpInside];
+    
+    NSString *token = [[NSUserDefaults standardUserDefaults] stringForKey:@"token"];
+    if (token) {
+        [self.navigationController pushViewController:[MINTimelineTableViewController new] animated:NO];
+    }
 }
 
 - (void)loginButtonPressed {
     MINWebViewController *webViewController = [[MINWebViewController alloc] initWithSuccessBlock:^(NSString *token) {
-        NSLog(@"%@", token);
+        [[NSUserDefaults standardUserDefaults] setObject:token forKey:@"token"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+        [self.navigationController pushViewController:[MINTimelineTableViewController new] animated:YES];
     }];
     [self presentViewController:[[UINavigationController alloc] initWithRootViewController:webViewController] animated:YES completion:nil];
 }
--- minimagram/MINTimelineView.m
+++ minimagram/MINTimelineView.m
@@ -7,10 +7,13 @@
 //
 
 #import "MINTimelineView.h"
+#import "UIColor+minimagram.h"
+#import "UIFont+minimagram.h"
 
 @interface MINTimelineView()
 
 @property (nonatomic, strong) UIView *statusBarBackgroundView;
+@property (nonatomic, strong) UILabel *statusBarLabel;
 
 @end
 
@@ -23,8 +26,14 @@
         [self addSubview:self.tableView];
         
         self.statusBarBackgroundView = [UIView new];
-        self.statusBarBackgroundView.backgroundColor = [UIColor redColor];
+        self.statusBarBackgroundView.backgroundColor = [UIColor whiteColor];
         [self addSubview:self.statusBarBackgroundView];
+        
+        self.statusBarLabel = [UILabel new];
+        self.statusBarLabel.text = @"minimagr*m";
+        self.statusBarLabel.textColor = [UIColor minimagramBlueColor];
+        self.statusBarLabel.font = [UIFont minimagramBoldFontWithSize:12];
+        [self addSubview:self.statusBarLabel];
     }
     return self;
 }
@@ -33,7 +42,10 @@
     [super layoutSubviews];
     
     self.tableView.frame = self.frame;
-    self.statusBarBackgroundView.frame = CGRectMake(0, 0, self.frame.size.width, 20);
+    self.statusBarBackgroundView.frame = CGRectMake(0, 0, self.frame.size.width, 28);
+    
+    [self.statusBarLabel sizeToFit];
+    self.statusBarLabel.center = CGPointMake(self.center.x, self.statusBarBackgroundView.center.y);
 }
 
 @end
